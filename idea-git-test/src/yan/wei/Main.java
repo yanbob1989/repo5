fd9e2aa86ccc629b00b280b8f269158f93da6202
yan <yanw123@msn.com>
2020-11-04 20:57:07 +0800

    新建项目

---
+++ idea-git-test/src/yan/wei/Main.java
@@ -0,0 +1,14 @@
+package yan.wei;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("2331121313");
+        System.out.println("3424234234");
+        System.out.println("3424234234");
+        System.out.println("3424234234");
+        System.out.println("3424234234");
+        System.out.println("3424234234");
+    }
+}
